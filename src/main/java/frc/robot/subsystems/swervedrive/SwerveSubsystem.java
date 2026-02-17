99fdc73141f6760af19cb4f8929577d8bee0ccf9
Michael Wen <118504234+fwen2026@users.noreply.github.com>
2026-02-16 17:09:25 -0800

    new start pos

--- src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -87,9 +87,9 @@ public class SwerveSubsystem extends SubsystemBase {
    */
   public SwerveSubsystem(File directory) { 
     boolean blueAlliance = true;
-    Pose2d startingPose = blueAlliance ? new Pose2d(new Translation2d(Meter.of(4.69),
-                                                                      Meter.of(2.75)),
-                                                    Rotation2d.fromDegrees(90))
+    Pose2d startingPose = blueAlliance ? new Pose2d(new Translation2d(Meter.of(3.7),
+                                                                      Meter.of(4)),
+                                                    Rotation2d.fromDegrees(0))
                                        : new Pose2d(new Translation2d(Meter.of(12.9),
                                                                       Meter.of(7.15)),
                                                     Rotation2d.fromDegrees(270));
