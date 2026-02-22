01fa4ec240a85f64b1a1dd0a96bfe6908e35b43c
Aneya Patel <aneya.patel@gmail.com>
2026-02-22 14:08:22 -0800

    m

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -141,7 +141,7 @@ public final class Constants {
 
     public static final int HOOD_ABSOLUTE_ENCODER_ID = 22;
 
-    public static final double HOOD_ABSOLUTE_ENCODER_OFFSET = -0.645; // for the 15 deg offset
+    public static final double HOOD_ABSOLUTE_ENCODER_OFFSET = -0.504; // for the 15 deg offset
   }
 
   public static final class ShooterConstants{
