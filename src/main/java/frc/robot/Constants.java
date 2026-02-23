84b25e66e1425d5f10fd2fbb84972eb0553eded6
Michael Wen <118504234+fwen2026@users.noreply.github.com>
2026-02-22 17:56:39 -0800

    halved kP rotation

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -37,7 +37,7 @@ public final class Constants {
     public static final double kA = 0.173;
 
     public static final double kP_translation = 4.0;
-    public static final double kP_rotation = 4.0;
+    public static final double kP_rotation = 2.0;
 
     public static final double kI_translation = 0.0;
     public static final double kI_rotation = 0.0;
