6d8e86e16cd46391b30f3f417bf0471bd66cab8d
Michael Wen <118504234+fwen2026@users.noreply.github.com>
2026-02-22 18:19:52 -0800

    nuke

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,8 +16,6 @@ import edu.wpi.first.wpilibj.RobotBase;
 
 import swervelib.SwerveInputStream;
 import edu.wpi.first.math.geometry.Pose3d;
-import frc.robot.Constants.FieldConstants;
-import frc.robot.Constants.HoodConstants;
 import frc.robot.Constants.LimelightConstants;
 import frc.robot.Constants.OperatorConstants;
 import edu.wpi.first.networktables.GenericEntry;
@@ -29,23 +27,18 @@ import frc.robot.commands.periodic.SuperstructureCommand;
 import frc.robot.utility.Shooter.ShotCalculator;
 
 import frc.robot.subsystems.hood.Hood;
-import frc.robot.subsystems.hood.HoodIO;
 import frc.robot.subsystems.hood.HoodIOTalonFX;
 import frc.robot.subsystems.hood.HoodIOSim;
 import frc.robot.subsystems.shooter.Shooter;
-import frc.robot.subsystems.shooter.ShooterIO;
 import frc.robot.subsystems.shooter.ShooterIOTalonFX;
 import frc.robot.subsystems.shooter.ShooterIOSim;
 import frc.robot.subsystems.turret.Turret;
-import frc.robot.subsystems.turret.TurretIO;
 import frc.robot.subsystems.turret.TurretIOTalonFX;
 import frc.robot.subsystems.turret.TurretIOSim;
 import frc.robot.subsystems.feeder.Feeder;
-import frc.robot.subsystems.feeder.FeederIO;
 import frc.robot.subsystems.feeder.FeederIOTalonFX;
 import frc.robot.subsystems.feeder.FeederIOSim;
 import frc.robot.subsystems.intake.Intake;
-import frc.robot.subsystems.intake.IntakeIO;
 import frc.robot.subsystems.intake.IntakeIOTalonFX;
 import frc.robot.subsystems.intake.IntakeIOSim;
 import frc.robot.subsystems.swervedrive.SwerveSubsystem;
@@ -63,9 +56,9 @@ public class RobotContainer {
     "swerve/falcon"));
     private final VisionSubsystem vision;
 
-    private final Hood hood;
-    private final Shooter shooter;
-    private final Turret turret;
+    // private final Hood hood;
+    // private final Shooter shooter;
+    // private final Turret turret;
     private final Feeder feeder;
     private final Intake intake;
 
@@ -91,15 +84,15 @@ public class RobotContainer {
     public RobotContainer() {
       // Initialize subsystems with appropriate IO implementations
       if (RobotBase.isReal()) {
-        hood = new Hood(new HoodIOTalonFX());
-        shooter = new Shooter(new ShooterIOTalonFX());
-        turret = new Turret(new TurretIOTalonFX());
+        // hood = new Hood(new HoodIOTalonFX());
+        // shooter = new Shooter(new ShooterIOTalonFX());
+        // turret = new Turret(new TurretIOTalonFX());
         feeder = new Feeder(new FeederIOTalonFX());
         intake = new Intake(new IntakeIOTalonFX());
       } else {
-        hood = new Hood(new HoodIOSim());
-        shooter = new Shooter(new ShooterIOSim());
-        turret = new Turret(new TurretIOSim());
+        // hood = new Hood(new HoodIOSim());
+        // shooter = new Shooter(new ShooterIOSim());
+        // turret = new Turret(new TurretIOSim());
         feeder = new Feeder(new FeederIOSim());
         intake = new Intake(new IntakeIOSim());
       }
@@ -117,16 +110,16 @@ public class RobotContainer {
 
       vision = new VisionSubsystem(limelights);
 
-      SuperstructureCommand superstructureCommand = new SuperstructureCommand(
-        hood,
-        shooter,
-        turret,
-        drivebase::getPose,
-        drivebase::getFieldVelocity,
-        shotCalculator
-      );
+      // SuperstructureCommand superstructureCommand = new SuperstructureCommand(
+      //   hood,
+      //   shooter,
+      //   turret,
+      //   drivebase::getPose,
+      //   drivebase::getFieldVelocity,
+      //   shotCalculator
+      // );
 
-      hood.setDefaultCommand(superstructureCommand); // choose one of these to be default!
+      // hood.setDefaultCommand(superstructureCommand); // choose one of these to be default!
 
       configureBindings();
     }
