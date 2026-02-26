ad98b7385fecbfdcd9e3ccc61a8016d14617c336
Michael Wen <118504234+fwen2026@users.noreply.github.com>
2026-02-26 13:14:17 -0800

    ?

--- src/main/java/frc/robot/subsystems/shooter/ShooterIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIOTalonFX.java
@@ -7,7 +7,6 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
 import com.ctre.phoenix6.controls.NeutralOut;
-import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
