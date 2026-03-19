e15facdd7fef36965534cfaf1990a45860c3d973
Michael Wen <118504234+fwen2026@users.noreply.github.com>
2026-03-18 18:47:51 -0700

    hi

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -137,7 +137,7 @@ public class RobotContainer {
         passCalculator
       );
 
-      hood.setDefaultCommand(superstructureCommand);
+      // hood.setDefaultCommand(superstructureCommand);
 
       configureBindings();
     }
