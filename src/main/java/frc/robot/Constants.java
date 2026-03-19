a9238fa9b3fba9567f525b746567da3d1a79f6ac
Michael Wen <118504234+fwen2026@users.noreply.github.com>
2026-03-18 17:54:52 -0700

    hi

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -111,10 +111,10 @@ public final class Constants {
           break;
         case "limelight-cl":
           limelightPose = new Pose3d(
-            -0.293, // front/back. this is temporary.
-            -0.296, // left/right 
-            0.499, // height
-            new Rotation3d(0, Units.degreesToRadians(0.0), Units.degreesToRadians(180.0))
+            -0.2957, // front/back. this is temporary.
+            -0.2325, // left/right 
+            0.336, // height
+            new Rotation3d(0, Units.degreesToRadians(25.0), Units.degreesToRadians(90.0))
           );
           break;
         case "limelight-cr":
