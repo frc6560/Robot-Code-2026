2805ce8d2ea25f730d3541e05b12c75a43b4f0f6
Michael Wen <118504234+fwen2026@users.noreply.github.com>
2026-03-26 19:02:58 -0700

    wtf

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -244,11 +244,11 @@ public final class Constants {
     public static final boolean ABSOLUTE_ENCODER_REVERSED = false; 
 
     // soft limits
-    public static final double LOWER_SOFT_LIMIT = -80.0;
+    public static final double LOWER_SOFT_LIMIT = -100.0;
     public static final double UPPER_SOFT_LIMIT = 270.0;
 
     // Wire protection thresholds - prefer unwinding toward center when outside these bounds
-    public static final double WIRE_PROTECTION_LOWER = -80.0;
+    public static final double WIRE_PROTECTION_LOWER = -100.0;
     public static final double WIRE_PROTECTION_UPPER = 270.0;
   }
 
